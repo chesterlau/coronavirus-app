f91f6ac4640dc6bb117106171fd686e8473b6ab1
Chester Lau <chesterl@openpay.com.au>
2020-04-15 22:11:27 +1000

    Added react dev tools support

--- src/redux/Store/Store.js
+++ src/redux/Store/Store.js
@@ -1,6 +1,6 @@
 import {createStore} from 'redux';
 import Reducers from '../Reducers/Reducers';
 
-const Store = createStore(Reducers);
+const Store = createStore(Reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default Store;
